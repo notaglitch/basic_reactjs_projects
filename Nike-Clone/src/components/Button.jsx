a0e676a270fc603dfa6e163ed90c6f7b2d067501
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-19 20:13:14 +0100

    Updated the button component

--- Nike-Clone/src/components/Button.jsx
+++ Nike-Clone/src/components/Button.jsx
@@ -1,12 +1,17 @@
-const Button = ({ label, iconURL, }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
   return (
-    <button className="flex justify-center items-center gap-2 rounded-full border border-coral-red bg-coral-red px-7 py-4 font-montserrat text-lg leading-none text-white shadow-coral-red">
+    <button className={`flex justify-center items-center gap-2 
+    rounded-full border border-coral-red 
+    bg-coral-red px-7 py-4 font-montserrat text-lg
+     leading-none
+    ${backgroundColor ? `${backgroundColor} ${textColor} 
+    ${borderColor}` : 'text-white'}`}>
         {label}
 
         {iconURL && <img src={iconURL} alt="arrow right icon" 
         className="ml-2 rounded-full w-5 h-5"/>}
     </button>
-)
+  )
 }
 
 export default Button
