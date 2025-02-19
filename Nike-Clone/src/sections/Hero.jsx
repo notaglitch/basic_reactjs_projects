b497f8343a6f255e79ab1b1872393df31bcec93e
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-19 16:17:22 +0100

    Updated HERO section

--- Nike-Clone/src/sections/Hero.jsx
+++ Nike-Clone/src/sections/Hero.jsx
@@ -4,26 +4,39 @@ import { statistics } from "../constants";
 
 const Hero = () => {
   return (
-    <section id="home" className="w-full flex xl:flex-row min-h-screen gap-10 max-container">
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
-        <p className="text-[24px] font-bold text-coral-red">Our Summer Collection</p>
-        <h1 className="mt-10 font-palanquin text-[52px] font-bold lg:max-w-xl">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The new arrival</span>
-          <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span>
-          Shoes
-        </h1>
-        <p className="mt-6 lg:max-w-lg info-text">Discover the best Nike shoes for your performance. Explore our latest collection.</p>
-          <Button label="Shop Now" iconURL={arrowRight} />
-          <div className="flex justify-start items-start flex-wrap w-full gap-16 mt-20">
-            {statistics.map((statistic) => (
-              <div key={statistic.label}>
-                <p className="text-[20px] font-bold text-coral-red">{statistic.value}</p>
-                <p className="leading-7 text-[20px] font-semibold">{statistic.label}</p>
-              </div>
-            ))}
-          </div>
+    <section
+      id='home'
+      className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
+    >
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+        <p className='text-xl font-montserrat text-coral-red'>
+          Our Summer collections
+        </p>
 
+        <h1 className='mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+          <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
+            The New Arrival
+          </span>
+          <br />
+          <span className='text-coral-red inline-block mt-3'>Nike</span> Shoes
+        </h1>
+        <p className='font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm'>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+
+        <Button label='Shop now' iconURL={arrowRight} />
+
+        <div className='flex justify-start items-start flex-wrap w-full mt-20 gap-16'>
+          {statistics.map((stat, index) => (
+            <div key={index}>
+              <p className='text-4xl font-palanquin font-bold'>{stat.value}</p>
+              <p className='leading-7 font-montserrat text-slate-gray'>
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>   
     </section>
   )
