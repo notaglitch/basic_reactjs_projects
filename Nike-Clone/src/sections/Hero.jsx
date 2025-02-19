d323372b437a6b368f18e144b0bd15ea8133eafc
Ghafoor Nazari <GhafoorNazari@proton.me>
2025-02-19 17:01:16 +0100

    Fixed photo-changing bug

--- Nike-Clone/src/sections/Hero.jsx
+++ Nike-Clone/src/sections/Hero.jsx
@@ -44,30 +44,30 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+      <div className='relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center'>
         <img
           src={bigShoeImg}
-          alt='shoe collection'
+          alt='shoe colletion'
           width={610}
-          height={500}
+          height={502}
           className='object-contain relative z-10'
-          />
+        />
 
-          <div className="flex sm:gap-6 gap-4 absolute -bottom-{5%} sm:left-[10%] 
-          max-sm:px-6">
-            {shoes.map((shoe) => (
-              <div key={shoe}>
-                <ShoeCard 
-                imgURL={shoe}
-                changeBigShoeImage={(shoe) => {setBigShoeImg(shoe)}}
+        <div className='flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6'>
+          {shoes.map((image, index) => (
+            <div key={index}>
+              <ShoeCard
+                index={index}
+                imgURL={image}
+                changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
-                />
-              </div>
-            ))}
-          </div>
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
 export default Hero
